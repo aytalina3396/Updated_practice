bc5fb1b9d3c66cad2a5f7d16e2ab1878095eeed1
Aytalina Shaposhnikova <aytalinashaposhnicova@mail.ru>
2023-05-30 12:24:02 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,29 @@
-import './App.css';
-import Header from './containers/Headers';
-import ProductList from './containers/ProductList';
-
+//Для @reduxjs/toolkit
+import "./App.css";
+import Header from "./containers/Headers";
+import ProductList from "./slice/ProductList";
 
 function App() {
   return (
     <div className="App">
-        <Header />
-        <ProductList />
+      <Header />
+      <ProductList />
     </div>
   );
 }
 
 export default App;
 
+//Для redux react-redux
+// import "./App.css";
+// import Header from "./containers/Headers";
+// import ProductList from "./containers/ProductList";
 
 // function App() {
 //   return (
 //     <div className="App">
-//       <Router>
-//         <Header />
-//         <Routes>
-//           <Route path="/" exact component={ProductList} />
-//           <Route path="/product/:productId" component={ProductDetail} />
-//           <Route>404 Not Found!</Route>
-//         </Routes>
-//       </Router>
+//       <Header />
+//       <ProductList />
 //     </div>
 //   );
 // }
